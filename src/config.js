a12e19094572f190fdb174addbe28c6e50612f9a
Cedric Rabarijohn <cedrictianarabarijohn@gmail.com>
2023-03-13 17:48:30 +0300

    just a simple push to trigger deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
-export const OPENAI_KEY = `${import.meta.env.VITE_OPENAI_KEY}`
-export const OPENAI_URL = `https://api.openai.com/v1/completions`
+export const OPENAI_KEY = `${import.meta.env.VITE_OPENAI_KEY}`;
+export const OPENAI_URL = `https://api.openai.com/v1/completions`;
+// export const deploy = true;
 /*
 curl https://api.openai.com/v1/chat/completions -H 'Content-Type: application/json' -H 'Authorization: Bearer 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' -d '{ "model": "gpt-3.5-turbo", "messages": [{"role": "user", "content": "Hi"}], "temperature": 0.7 }'
 */
