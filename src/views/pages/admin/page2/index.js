8ea8b5c97f7deb0fc834c59662667eba215aa639
Tsantaniaina Rakotonjanahary <127739102+athackhiu@users.noreply.github.com>
2023-03-23 08:01:21 +0300

    go

--- src/views/pages/admin/page2/index.js
+++ src/views/pages/admin/page2/index.js
@@ -1,6 +1,7 @@
 
 import { Fragment, useEffect } from 'react'
 import { Row, Col, CardBody, CardText , CardTitle } from 'reactstrap'
+import GoogleMapReact from 'google-map-react';
 
 import Card from '@components/card-snippet'
 
@@ -10,7 +11,12 @@ const Page2 = () => {
     <Row>
       <Col md='6' sm='12'>
         <Card title={"go"} >
-          <h1>Page2</h1>
+          <h1>Map</h1>
+          <div style={{ height: '100vh', width: '100%' , borderRadius : '50%' }} >
+              <GoogleMapReact   bootstrapURLKeys = {{ key:  'AIzaSyBStuzSA7bW5n2wddHiCSNBG-TPWh-sd64' }} defaultCenter = {{ lat: -18.933333, lng: 47.516667 }} center = {{ lat: -18.933333, lng: 47.516667 }}  defaultZoom = {10} options = {{ mapTypeControlOptions: {  mapTypeId: "hybrid" }}}  >
+               
+              </GoogleMapReact>
+            </div>
         </Card>
       </Col>
     </Row>
